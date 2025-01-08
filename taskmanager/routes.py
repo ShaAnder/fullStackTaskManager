05484195115a73b5ef33d4f94e297453e8fedcda
Shaun Anderton <shaunanderton1@gmail.com>
2025-01-08 15:48:05 +0000

    add category viewing and editing

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,18 +10,41 @@ def home():
 def tasks():
     return render_template("tasks.html")
 
+#when user adds a category
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    #if method is post (user submitted information)
     if request.method == "POST":
+        #create a new category object with the category name being the request
         category = Category(category_name=request.form.get("category_name"))
-        # db.session.add(category)
-        # db.session.commit()
-        categories = db.session.query(Category)
-        for c in categories:
-            db.session.delete(c)
-            db.session.commit()
+        #add and commit it to the session
+        db.session.add(category)
+        db.session.commit()
+        # categories = db.session.query(Category)
+        # for c in categories:
+        #     db.session.delete(c)
+        #     db.session.commit()
         return redirect(url_for("home"))
-    return render_template("add-category.html")
+    #we create a cursor object (array) that we can pass to the template
+    view_categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("add-category.html", categories=view_categories)
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("add_category"))
+    return render_template("edit-category.html", category=category)
+    
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    view_categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("add-category.html", categories=view_categories)
 
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
